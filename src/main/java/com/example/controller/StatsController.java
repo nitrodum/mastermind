142a0a67be9786487805f5ef7e9a80549bf59680
Nima_Lama <nitrodum@yahoo.com>
2025-08-27 23:52:01 -0700

    Added daily stats to display

--- src/main/java/com/example/controller/StatsController.java
+++ src/main/java/com/example/controller/StatsController.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
+import com.example.model.DailyStats;
 import com.example.model.User;
+import com.example.model.UserStats;
 import com.example.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/stats")
@@ -20,19 +23,34 @@ public class StatsController {
     }
 
     @GetMapping("")
-    public String statsPage(HttpSession session, Model model) {
+    public String statsPage(@RequestParam(defaultValue = "infinite") String mode, HttpSession session, Model model) {
         User  user = (User) session.getAttribute("user");
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("statsMode", mode);
 
         if (user == null) {
             return "redirect:/auth";
         }
 
+        if (mode.equals("daily")) {
+            DailyStats dailyStats = user.getDailyStats();
+
+            if (user.getDailyStats() == null) {
+               dailyStats = new DailyStats(user);
+            }
+            model.addAttribute("currentStats", dailyStats);
+        } else {
+
+            UserStats userStats = user.getUserStats();
+            if (userStats == null) {
+                userStats = new UserStats(user);
+            }
+            model.addAttribute("currentStats", userStats);
+            }
+
         if (user.getUserStats() != null) {
             session.setAttribute("userStats", user.getUserStats());
         }
-
-        model.addAttribute("username", user.getUsername());
-
         return "stats";
     }
 
